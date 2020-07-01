aad16841212bd84b8c0da42b15ff678e561ca37f
Deven Sitapara <shriramsoft@gmail.com>
2020-07-01 19:01:46 +0530

    npm start  fix

--- client/src/index.js
+++ client/src/index.js
@@ -7,13 +7,6 @@ import { Store } from "./Redux/Store/Store";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 
-//Fake
-//import { configureFakeBackend } from "./Helper/fake-backend";
-
-//serviceWorker.register();
-
-//configureFakeBackend();
-
 ReactDOM.render(
   <Provider store={Store}>
     <HashRouter>
@@ -22,8 +15,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
